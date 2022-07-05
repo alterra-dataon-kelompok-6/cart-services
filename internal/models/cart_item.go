ba8e1384585ff94c46344a62d53fd34a51bfe7ed
Sahal Mahfudh <sahalmahfudh07@gmail.com>
2022-07-05 20:42:15 +0700

    fix(model): update model cart add index on customer_id and deleted_at

--- internal/models/cart_item.go
+++ internal/models/cart_item.go
@@ -2,7 +2,7 @@ package model
 
 type CartItem struct {
 	Common
-	CartID    uint `json:"cart_id" gorm:"index:idx_cart,unique"`
-	ProductID uint `json:"product_id" gorm:"index:idx_cart,unique"`
+	CartID    uint `json:"cart_id" gorm:"index:idx_cart_item,unique"`
+	ProductID uint `json:"product_id" gorm:"index:idx_cart_item,unique"`
 	Qty       uint `json:"qty"`
 }
