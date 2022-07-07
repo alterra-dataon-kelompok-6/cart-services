4b310229d3d7e9f8a364241030c6b85c3f136139
Sahal Mahfudh <sahalmahfudh07@gmail.com>
2022-07-07 22:42:50 +0700

    feat(api): add api request to product services and init api request to customer services

---
+++ libs/api/customer.go
@@ -0,0 +1,8 @@
+package api
+
+// type CustomerResponseApi struct {
+// }
+
+// func GetCustomer(id uint) interface{} {
+// 	return 0
+// }
---
+++ libs/api/product.go
@@ -0,0 +1,62 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"product-services/libs/env"
+)
+
+type Category struct {
+	Category   string `json:"category"`
+	CategoryID uint   `json:"category_id"`
+}
+
+type Product struct {
+	ID          uint   `json:"id"`
+	CategoryID  int    `json:"category_id"`
+	Name        string `json:"name"`
+	Stock       uint   `json:"stock"`
+	Price       uint   `json:"price"`
+	Image       string `json:"image"`
+	Description string `json:"description"`
+}
+
+type Data struct {
+	Category Category `json:"category"`
+	Product  Product  `json:"product"`
+}
+
+type ProductResponseApi struct {
+	Data   Data `json:"data"`
+	Status bool `json:"status"`
+}
+
+var productBaseUrl = env.GetEnv("URL_PRODUCT_SERVICES")
+
+func GetProduct(id uint) *ProductResponseApi {
+	url := fmt.Sprintf("%v/products/%d", productBaseUrl, id)
+	log.Println(url)
+
+	// resp, err := http.NewRequest(http.MethodGet, url, nil)
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	var data ProductResponseApi
+	json.Unmarshal(body, &data)
+
+	log.Println("resp", resp, "data", data)
+	return &data
+}
